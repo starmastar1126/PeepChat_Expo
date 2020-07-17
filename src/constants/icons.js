ee66b72f8a45f1b660b448a3e7095ddc6c344395
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-17 03:57:02 +0200

    zother

--- src/constants/icons.js
+++
@@ -1,8 +0,0 @@
-export const tabBarIcons = {
-    NewsFeed: '@3xhand-leaf-(1)',
-    Info: '@3xhand-leaf-(1)',
-    Peep: 'multiple-users-silhouette@3x',
-    Notifications: 'alarm',
-    Profile: 'user',
-    ToDo: 'ios-list-box',
-};
--- src/stores/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import todo_reducer from './todo_reducer';
-
-export default combineReducers({
-    todo_reducer,
-});
